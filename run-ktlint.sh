e5bee17da6005bb3c87dfde967a3db83869404d3
Jonathan Maltz <maltz@yelp.com>
2019-01-29 09:39:57 +0530

    add a space

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -8,6 +8,6 @@ then
 fi
 cd ..
 
-changed_kotlin_files=$(git diff --cached --name-only --diff-filter=ACMR | grep".*kt$" )
+changed_kotlin_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*kt$" )
 echo $changed_kotlin_files
 .cache/ktlint $changed_kotlin_files
