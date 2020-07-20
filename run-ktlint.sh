2e55386fd845e4494769faa747d5df8c5269469e
John Wood <john@verisart.com>
2020-07-20 16:19:21 +0100

    Use latest version and also fix problems

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -3,11 +3,11 @@ mkdir -p .cache
 cd .cache
 if [ ! -f ktlint ]
 then
-    curl -sSLO https://github.com/shyiko/ktlint/releases/download/0.29.0/ktlint
+    curl -sSLO https://github.com/shyiko/ktlint/releases/download/0.37.2/ktlint
     chmod 755 ktlint
 fi
 cd ..
 
 changed_kotlin_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*kt$" )
 echo $changed_kotlin_files
-.cache/ktlint $changed_kotlin_files
+.cache/ktlint -F $changed_kotlin_files
