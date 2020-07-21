159932dadb52623714af258ddd6987f89a71ba5d
John Wood <john@verisart.com>
2020-07-21 11:20:21 +0100

    Fix issue with github workspaces

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -4,6 +4,8 @@ cd .cache
 if [ ! -f ktlint ]
 then
     curl -sSLO https://github.com/shyiko/ktlint/releases/download/0.37.2/ktlint
+    # Needed for https://github.com/moby/moby/issues/9547 ?
+    sync
     chmod 755 ktlint
 fi
 cd ..
