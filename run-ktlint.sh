2ee7e37455ae9b0835d7e3619be0b58313d9999c
John Wood <john@verisart.com>
2020-08-17 16:42:45 +0100

    Replace brittle download with just keeping the ktlint file in the git repo

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env sh
 set -e
-mkdir -p .cache
-cd .cache
-if [ ! -f ktlint ]
-then
-    curl -sSL -o ktlint$$.tmp https://github.com/shyiko/ktlint/releases/download/0.37.2/ktlint
-    cp ktlint$$.tmp ktlint
-    # Needed for https://github.com/moby/moby/issues/9547 ?
-    sync
-    chmod 755 ktlint
-fi
-cd ..
-
-.cache/ktlint -F $@ && git add $@
+./ktlint -F $@ && git add $@
 
